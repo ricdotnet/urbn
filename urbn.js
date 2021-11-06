80d24b2a841626e3e84f6c566c3aa0442c22fc98
Ricardo Rocha <me@rrocha.uk>
2021-11-06 18:41:19 +0000

    :fire: some updates;

--- urbn.js
+++ urbn.js
@@ -15,36 +15,84 @@ class UrbnDictionary {
    */
   getFirst(term) {
     return new Promise((resolve, reject) => {
-      http.get(`${this._api}${term}`, (res) => {
+      http.get(this._api + term, (res) => {
         let data = '';
 
         if (typeof term === 'undefined') {
-          reject({error: res.statusCode, message: 'No term defined.'})
+          reject(this.sendError('undefined', res.statusCode))
         }
 
         if (res.statusCode !== 200) {
-          reject({code: res.statusCode, message: 'Some error occurred, try again.'});
+          reject(this.sendError('not200', res.statusCode))
         }
 
-        res.on('error', (error) => {
-          reject({error: error.message})
+        res.on('error', error => {
+          reject(this.sendError('generic', null, error))
         })
 
-        res.on('data', (chunk => {
+        res.on('data', chunk => {
           data += chunk;
-        }))
-        res.on('end', () => {
+        }).on('end', () => {
           try {
             resolve(JSON.parse(data).list[0])
           } catch (error) {
             reject({error: error})
           }
         })
-      }).on('error', (error) => {
-        reject({error: error.message})
       })
     })
   }
+
+  /**
+   * Get all results from page 1.
+   * @param term
+   * @return {Promise<Object>}
+   */
+  getAll(term) {
+    return new Promise((resolve, reject) => {
+      http.get(this._api + term, (res) => {
+        let data = '';
+
+        if (typeof term === undefined) {
+          reject(this.sendError('undefined', res.statusCode));
+        }
+
+        if (res.statusCode !== 200) {
+          reject(this.sendError('not200', res.statusCode));
+        }
+
+        res.on('error', error => {
+          reject(this.sendError('generic', null, error))
+        })
+
+        res.on('data', chunk => {
+          data += chunk;
+        }).on('end', () => {
+          try {
+            resolve(JSON.parse(data).list)
+          } catch (error) {
+            reject(this.sendError('generic', null, error))
+          }
+        })
+      })
+    })
+  }
+
+  sendError(type, statusCode, error) {
+
+    if (type === 'undefined') {
+      return {error: statusCode, message: 'No term defined.'};
+    }
+
+    if (type === 'not200') {
+      return {code: statusCode, message: 'Some error occurred, try again.'};
+    }
+
+    if (type === 'generic') {
+      return {message: error.message}
+    }
+  }
+
 }
 
 function Urbn() {
